d09f099391e17c6cd1a995921b8e64764ca6e9b3
Cong Zhao <zhaocong@apache.org>
2024-05-14 18:50:28 +0800

    Update PulsarStandaloneStarter.java

--- pulsar-broker/src/main/java/org/apache/pulsar/PulsarStandaloneStarter.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/PulsarStandaloneStarter.java
@@ -37,6 +37,8 @@ import picocli.CommandLine.Option;
 @Slf4j
 public class PulsarStandaloneStarter extends PulsarStandalone {
 
+    // test 3.3 ci
+
     private static final String PULSAR_CONFIG_FILE = "pulsar.config.file";
 
     @Option(names = {"-g", "--generate-docs"}, description = "Generate docs")
